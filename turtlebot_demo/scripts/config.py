b03ce065c797cbc637df3cf11f507831dcc07b44
Esssssa <81052637+Esssssa@users.noreply.github.com>
2024-12-22 18:59:55 +0200

    Delete turtlebot_demo directory

--- turtlebot_demo/scripts/config.py
+++
@@ -1,8 +0,0 @@
-CONFIG = {
-    "lower_yellow": [20, 100, 100],
-    "upper_yellow": [30, 255, 255],
-    "base_linear_speed": 0.2,
-    "min_linear_speed": 0.05,
-    "angular_speed": 0.5,
-    "slowdown_margin": 0.25,
-}
